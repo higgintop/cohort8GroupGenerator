cc2e73ef221207baad5ae076303c9b601eed7886
Allison Higginbotham <higgintop@gmail.com>
2015-02-02 10:57:33 -0600

    added an init function that is called when document is ready

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
-var $input = $('input'),
-    $ul    = $('ul'),
+var $input,
+    $ul,
     url   = 'https://yspuku7qvh9u4cr3.firebaseio.com/.json';
 
-$input.change(getUpdateAndSplit);
-document.addEventListener('DOMContentLoaded', getUpdateAndSplit);
+// event listener - wait for DOM content to load
+// and then call init function
+// document.addEventListener('DOMContentLoaded', init);
+$(document).ready(init);
+
+
+// initialization function
+function init() {
+  $input = $('input');
+  $ul    = $('ul');
+
+  $input.change(getUpdateAndSplit);
+  getUpdateAndSplit();
+}
 
 function getUpdateAndSplit(){
   var count = $input.val();
-
   // clear contents of ul
   $ul.empty();
   // ajax call
